bd02b417d4d736ad882c3e66deda903ce5744233
sumina <suminaawa123@gmail.com>
2024-12-27 17:51:53 +0545

    API fetched

---
+++ postgresql.sql
@@ -0,0 +1,12 @@
+
+SELECT *
+FROM "stops"
+inner join "route_stops"
+on "stops"."id"= "route_stops"."stops_id"
+where "route_id" in  (
+    SELECT DISTINCT route_id 
+    FROM "route_stops"
+    INNER JOIN "stops"
+    ON "route_stops"."stops_id" = "stops"."id"
+    WHERE ("route_stops"."stops_id" = 2 OR "route_stops"."stops_id" = 4)
+);
